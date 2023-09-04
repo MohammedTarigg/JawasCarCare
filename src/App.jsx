151a5c4e628116883e57f9451945dd50173a53b3
Mohammed Tarig <m7md.tarigg@gmail.com>
2023-09-04 06:47:32 +0300

    useless comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Layout from "./pages/Layout";
 import "./App.css";
-import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   // const [selectedLanguage, setSelectedLanguage] = useState(i18n.language); // i18n.language contains the language assigned to lng in i18n.js file.
   return (
     <>
       <CssBaseline />
-      <Routes>
-        <Route path="/" exact element={<Layout />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <Layout />
     </>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,13 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Suspense fallback={<div>Loading</div>}>
-        <App />
-      </Suspense>
-    </BrowserRouter>
+    <Suspense fallback={<div>Loading</div>}>
+      <App />
+    </Suspense>
   </React.StrictMode>
 );
 
