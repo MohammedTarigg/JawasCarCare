817cae0cc18bf35abf1e86a6842e21bc274a382d
Mohammed Tarig <m7md.tarigg@gmail.com>
2023-10-19 09:07:38 +0300

    enable analytics

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "GTM-ZD7MVKGV4F",
+};
+
+TagManager.initialize(tagManagerArgs);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
