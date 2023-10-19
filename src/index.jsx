bbde5a70c0afc5f42ae32b55d67943eb966a6e13
Mohammed Tarig <m7md.tarigg@gmail.com>
2023-10-19 12:27:44 +0300

    enable analytics

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,20 @@ import ReactGA from "react-ga";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// // TODO: Replace the following with your app's Firebase project configuration
+// // See: https://firebase.google.com/docs/web/learn-more#config-object
+// const firebaseConfig = {
+//   // ...
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// // Initialize Analytics and get a reference to the service
+// const analytics = getAnalytics(app);
+
 ReactGA.initialize("G-ZD7MVKGV4F", { debug: true });
 
 root.render(
