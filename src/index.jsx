32d0e7c4d0dd99eec35e2081a687282e04017c11
Mohammed Tarig <m7md.tarigg@gmail.com>
2023-10-19 10:07:36 +0300

    enable analytics

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,12 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-import TagManager from "react-gtm-module";
-
-const tagManagerArgs = {
-  gtmId: "GTM-ZD7MVKGV4F",
-};
-
-TagManager.initialize(tagManagerArgs);
+import ReactGA from "react-ga";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+ReactGA.initialize("G-ZD7MVKGV4F", { debug: true });
+
 root.render(
   <React.StrictMode>
     <Suspense fallback={<div>Loading</div>}>
